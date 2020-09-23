775d13e029cfd02c3260f77fd9b919fd0f164cbe
jimmyawel <71686445+jimmyawel@users.noreply.github.com>
2020-09-23 13:22:17 +0300

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,22 +1,23 @@
 <?php
-    if(isset($_POST['submit'])){
-       $name=$_POST['name'];
-       $email=$_POST['email'];
-       $phone=$_POST['phone'];
-       $msg=$_POST['msg'];
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
 
-       $to='jimmylife10@gmail.com';
-       $subject='Form Submission';
-       $message="Name: ".$name."\n"."Phone: ".$phone."\n". "Wrote the following: "."\n\n".$msg;
-       $headers="From: ".$email;
+		$to='jimmylife10@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
 
-       if(mail($to, $subject, $message, $headers)){
-        echo "<h1> Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-       }
-       else{
-           echo "Something went wrong!";
-        }
-    }  
-?>     
-      
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
+
+     
   
